47b08624b054a45ef45112a8f37315ebbb5a428e
Eric Laursen <ericjlaursen@gmail.com>
2020-09-19 17:17:30 -0400

    more examples

--- scratch.js
+++ scratch.js
@@ -1,3 +1,4 @@
+
 // const add = (a,b) => a + b
 // const addLess = ([a,b]) => a + b
 
@@ -39,28 +40,49 @@
 
 
 // Currying part 2
+// const curried = fn => firstArg => secondArg => fn(firstArg, secondArg)
+
+// // returns the remainder of a divison
+// const curriedModulo = curried((x,y) => y % x)
+
+// /*
+// 	applies first arg to modulo to 2, 
+// 	returning 1 or 0 for an even or odd number
+// 	THIS IS THE FIRST EXAMPEL OF CURRYING IN ACTION HERE!
+// */
+// const isOdd = curriedModulo(2)
+
+// /*
+// 	applies a fn to an arr, 
+// 	 but allows for 2 fn calls 
+// 	  to make this happen via the curried fn
+// */ 
+// const curriedFilter = curried((filterFnParam, arr) => arr.filter(filterFnParam))
+
+// const getOddNumbers = curriedFilter(isOdd)
+
+// const res = getOddNumbers([1,2,3,4,5,6,7,8,9])
+
+
+
+
+
+
+// Currying part 3
 const curried = fn => firstArg => secondArg => fn(firstArg, secondArg)
 
-// returns the remainder of a divison
-const curriedModulo = curried((x,y) => y % x)
-
-/*
-	applies first arg to modulo to 2, 
-	returning 1 or 0 for an even or odd number
-	THIS IS THE FIRST EXAMPEL OF CURRYING IN ACTION HERE!
-*/
-const isOdd = curriedModulo(2)
-
-/*
-	applies a fn to an arr, 
-	 but allows for 2 fn calls 
-	  to make this happen via the curried fn
-*/ 
-const curriedFilter = curried((filterFnParam, arr) => arr.filter(filterFnParam))
-
-const getOddNumbers = curriedFilter(isOdd)
-
-const res = getOddNumbers([1,2,3,4,5,6,7,8,9])
-console.log('res')
+const uncurrieWords = str => str.split(' ');
+const testingString = 'something wicked this way comes'
+const res = uncurrieWords(testingString);
+console.log('uncurriedres')
 console.log(res)
+
+const curriedWords = splitParam => str => str.split(splitParam);
+const splitAtSpaces = curriedWords(' ')
+
+const curRes = splitAtSpaces(testingString)
+console.log('curRes')
+console.log(curRes)
+
+
 
