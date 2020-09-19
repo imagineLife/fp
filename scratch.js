87d6b69398bcaee752fc0965e94b1e5907069681
Eric Laursen <ericjlaursen@gmail.com>
2020-09-19 16:28:39 -0400

    includes more

--- scratch.js
+++ scratch.js
@@ -26,13 +26,41 @@
 
 
 // currying
-const add = (a,b) => a + b;
+// const add = (a,b) => a + b;
 
+// const curried = fn => firstArg => secondArg => fn(firstArg, secondArg)
+
+// const curriedAdd = curried(add)
+// const incFour = curriedAdd(4)
+
+// const res = incFour(7)
+// console.log('res')
+// console.log(res)
+
+
+// Currying part 2
 const curried = fn => firstArg => secondArg => fn(firstArg, secondArg)
 
-const curriedAdd = curried(add)
-const incFour = curriedAdd(4)
+// returns the remainder of a divison
+const curriedModulo = curried((x,y) => y % x)
 
-const res = incFour(7)
+/*
+	applies first arg to modulo to 2, 
+	returning 1 or 0 for an even or odd number
+	THIS IS THE FIRST EXAMPEL OF CURRYING IN ACTION HERE!
+*/
+const isOdd = curriedModulo(2)
+
+/*
+	applies a fn to an arr, 
+	 but allows for 2 fn calls 
+	  to make this happen via the curried fn
+*/ 
+const curriedFilter = curried((filterFnParam, arr) => arr.filter(filterFnParam))
+
+const getOddNumbers = curriedFilter(isOdd)
+
+const res = getOddNumbers([1,2,3,4,5,6,7,8,9])
 console.log('res')
 console.log(res)
+
